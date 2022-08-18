ac27a1090039ebd64710515024e10fb6d94b8a15
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-18 15:29:32 +0300

    commit tree 2af1086f5665e320d3888e3eebf82da1564b68b0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+#Take the list of the given strings
+#for each  of the words in the strings, break it into an array of its constituent  characters
+#Then sort the array of the constituent characters
+#Take the word passed at initalization
+#break it too in its contituent characters and sort the array of the cinstituent characters.
+#for each array of the constituent word characters, compare with the array of the constituent characters of the word passed at initialization
+#for each found, add it to an array
+#return the array
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    
+    def match(possible_anagrams)
+        anagrams = []
+        possible_anagrams.each do |possible_anagram|
+            if possible_anagram.chars.sort == @word.chars.sort
+            anagrams << possible_anagram
+            end
+        end
+        anagrams
+    end
+end
+
+anagram1 = Anagram.new("listen")
+pp anagram1.match(%w[enlists google inlets banana])
